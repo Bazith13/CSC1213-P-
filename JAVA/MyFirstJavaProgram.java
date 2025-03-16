ea24ccc1356ee39a20e6d083b23f2bd9c77b3d6c
Bazith13 <abdulbazith683@gmail.com>
2025-03-16 14:32:38 +0530

    Delete JAVA directory

--- JAVA/MyFirstJavaProgram.java
+++
@@ -1,9 +0,0 @@
-//class<class_name>
-class MyFirstJavaProgram{
-public static void main(String[] args)
-{
-	System.out.println(" MyFirstJavaProgram in Java");
-	System.out.println(" Hello Java Developers !!!");
-	
-}// end of the main
-}// end of the class.
