2bb48644217cb42c8ee8222445fdc68bfafffe35
Bazith13 <abdulbazith683@gmail.com>
2025-03-16 14:32:06 +0530

    Add files via upload

---
+++ Day01/Details.java
@@ -0,0 +1,8 @@
+class Details {
+
+public static void main (String[] args) {
+
+	System.out.println("First Name: THOWFEEK \nLast Name: ABDUL BAZITH \nAddress : GM Manzoor Road Nintavur:-13");
+
+}
+}
---
+++ Day01/MyFirstJavaProgram.java
@@ -0,0 +1,9 @@
+//class<class_name>
+class MyFirstJavaProgram{
+public static void main(String[] args)
+{
+	System.out.println(" MyFirstJavaProgram in Java");
+	System.out.println(" Hello Java Developers !!!");
+	
+}// end of the main
+}// end of the class.
